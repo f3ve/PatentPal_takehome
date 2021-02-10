7567c6548f01736768feceed268ae4f33342c289
f3ve <free.eve@pm.me>
2021-02-09 17:29:33 -0800

    added top bar stylings

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <div id="app">
-    <nav id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/newpage">New Page</router-link>
-    </nav>
+    <topbar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Topbar from './components/topbar.vue';
+export default {
+  name: "App",
+  components: {
+    Topbar
+  }
+}
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
+
 
 #nav {
   padding: 30px;
--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    <h1>Topbar</h1>
+  <div class="topbar">
+    <router-link to="/">
+      <img src="../assets/PatentPal_Logo.png" alt="PatentPal logo" class="mainLogo">
+    </router-link>
+    <nav id="nav">
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/newpage">New Page</router-link>
+    </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Topbar"
+  name: "Topbar",
+  data: () => ({
+    title: "poop",
+    bool: false
+  })
 }
 </script>
 
 <style scoped>
-  
+  div.topbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 64px;
+  }
+
+  img.mainLogo {
+    width: 100%;
+    max-height: 40px;
+  }
+
+  #nav {
+  padding: 30px;
+}
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #e04c4c;
+  }
 </style>
