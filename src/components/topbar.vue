1667c95674476a3a4c51072960962ce5bed2da30
f3ve <free.eve@pm.me>
2021-02-09 16:48:47 -0800

    inital commit

---
+++ src/components/topbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Topbar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topbar"
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ src/views/NewPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is a new page!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewPage'
+}
+</script>
